8d91b975496fe763e2ff83be44213c67a609ba04
John K. Feser <jack.feser@gmail.com>
2018-03-15 14:58:25 -0400

    Fix bugs in test script.

--- bin/test.py
+++ bin/test.py
@@ -8,7 +8,7 @@ import sys
 COMPILE = '/scratch/jack/_build/default/fastdb/bin/compile.exe'
 
 def main(compile_args, out_dir, result_db, fn):
-    conn = sqlite3.connect(result_db)
+    result_db = os.getcwd() + '/' + result_db
     fn = os.getcwd() + '/' + fn
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
@@ -23,8 +23,9 @@ def main(compile_args, out_dir, result_db, fn):
     db_size = None
     exe_size = None
     try:
-        cmd = '{} {} {} > compiler.log'.format(COMPILE, compile_args, fn)
-        subprocess.check_call(cmd, shell=True)
+        cmd = '{} {} {}'.format(COMPILE, compile_args, fn)
+        with open('compile.log', 'w') as f:
+            subprocess.check_call(cmd, shell=True, stdout=f, stderr=f)
         try:
             subprocess.check_call('./scanner.exe -p db.buf > output.csv', shell=True)
             out = subprocess.check_output('./scanner.exe -t 10 db.buf', shell=True)
@@ -36,6 +37,7 @@ def main(compile_args, out_dir, result_db, fn):
     except subprocess.CalledProcessError:
         compile_failed = True
 
+    conn = sqlite3.connect(result_db, timeout=10)
     c = conn.cursor()
     c.execute('create table if not exists results (name text, runs_per_sec numeric, db_size numeric, exe_size numeric, compile_failed integer, scanner_failed integer)')
     c.execute('insert into results values (?, ?, ?, ?, ?, ?)', (name, runtime, db_size, exe_size, compile_failed, scanner_failed))
