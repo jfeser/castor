b3ca6e2806fc8b98e536c6df152bbd3053a18407
John K. Feser <jack.feser@gmail.com>
2018-03-19 21:09:29 -0400

    Tweak benchmark to match what we actually run.

--- bench/query96-postgres.sql
+++ bench/query96-postgres.sql
@@ -1,11 +1,11 @@
 -- set seed to 0;
--- create temp table store_sales as (select * from store_sales order by random() limit 10000);
+-- create temp table store_sales as (select * from store_sales order by random() limit 500000);
 -- set seed to 0;
--- create temp table household_demographics as (select * from household_demographics order by random() limit 10000);
+-- create temp table household_demographics as (select * from household_demographics order by random() limit 500000);
 -- set seed to 0;
--- create temp table time_dim as (select * from time_dim order by random() limit 10000);
+-- create temp table time_dim as (select * from time_dim order by random() limit 500000);
 -- set seed to 0;
--- create temp table store as (select * from store order by random() limit 10000);
+-- create temp table store as (select * from store order by random() limit 500000);
 
 -- Run with no view and no indexes.
 select count(*) from store_sales, household_demographics, time_dim, store
@@ -15,7 +15,7 @@ and ss_store_sk = s_store_sk
 and time_dim.t_hour = 20
 and time_dim.t_minute >= 30
 and household_demographics.hd_dep_count = 0
-and store.s_store_name = 'ese'
+-- and store.s_store_name = 'ese'
 order by count(*);
 
 explain analyze select count(*) from store_sales, household_demographics, time_dim, store
@@ -25,7 +25,7 @@ and ss_store_sk = s_store_sk
 and time_dim.t_hour = 20
 and time_dim.t_minute >= 30
 and household_demographics.hd_dep_count = 0
-and store.s_store_name = 'ese'
+-- and store.s_store_name = 'ese'
 order by count(*);
 
 -- Run with view but no indexes.
@@ -35,7 +35,8 @@ where ss_sold_time_sk = time_dim.t_time_sk
 and ss_hdemo_sk = household_demographics.hd_demo_sk
 and ss_store_sk = s_store_sk
 and time_dim.t_minute >= 30
-and store.s_store_name = 'ese');
+-- and store.s_store_name = 'ese'
+);
 
 select count(*) from v1 where
 v1.t_hour = 20
