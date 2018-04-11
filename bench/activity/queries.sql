d67d4dacd27105ddc570a4d4dcba934d11544c98
Jack Feser <feser@csail.mit.edu>
2018-04-11 15:46:28 -0400

    Extract and format queries.

--- bench/activity/queries.sql
+++ bench/activity/queries.sql
@@ -1,62 +1,284 @@
--- get activity counts per day for one subject, with 0 count if no activity present
-select timeline, activity_level_id, count(activity_sleep) as activity_minutes from
-       (select distinct(date_trunc('day',start_time)) as timeline,
-               a.id as activity_level_id from subject_device_activity_level,
-               activity_level a
-        where subject_device_id = :subjectDeviceId and a.id < 6) as t
-        left join subject_device_activity_level on
-        date_trunc('day',start_time)=timeline and
-        activity_level_id=activity_sleep and
-        subject_device_id = :subjectDeviceId
-group by timeline, activity_level_id order by timeline;
+-- get activity counts per day for one subject, with 0 count if no activity
+-- present
+SELECT
+          timeline
+        , activity_level_id
+        , COUNT(activity_sleep) AS activity_minutes
+FROM
+          (
+                    SELECT DISTINCT
+                              (date_trunc('day',start_time)) AS timeline
+                            , a.id                           AS activity_level_id
+                    FROM
+                              subject_device_activity_level
+                            , activity_level a
+                    WHERE
+                              subject_device_id = :subjectDeviceId
+                              AND a.id          < 6
+          )
+          AS t
+          LEFT JOIN
+                    subject_device_activity_level
+          ON
+                    date_trunc('day',start_time)=timeline
+                    AND activity_level_id       =activity_sleep
+                    AND subject_device_id       = :subjectDeviceId
+GROUP BY
+          timeline
+        , activity_level_id
+ORDER BY
+          timeline
+;
 
--- get activity counts per day for one subject, with 0 count if no activity present
-	@Query(value="select timeline,cast(count(activity_sleep) as INTEGER) as activity_minutes from "
-			+ "(select distinct(date_trunc('day',start_time)) as timeline, a.id as activity_level_id from subject_device_activity_level, "
-			+ "activity_level a where subject_device_id = :subjectDeviceId and a.id = :activityLevelId) as t left join subject_device_activity_level on "
-			+ "date_trunc('day',start_time)=timeline and activity_level_id=activity_sleep and subject_device_id = :subjectDeviceId "
-			+ "group by timeline, activity_level_id order by timeline",
+-- get activity counts per day for one subject, with 0 count if no activity
+-- present
+SELECT
+          timeline
+        , CAST(COUNT(activity_sleep) AS INTEGER) AS activity_minutes
+FROM
+          (
+                    SELECT DISTINCT
+                              (date_trunc('day',start_time)) AS timeline
+                            , a.id                           AS activity_level_id
+                    FROM
+                              subject_device_activity_level
+                            , activity_level a
+                    WHERE
+                              subject_device_id = :subjectDeviceId
+                              AND a.id          = :activityLevelId
+          )
+          AS t
+          LEFT JOIN
+                    subject_device_activity_level
+          ON
+                    date_trunc('day',start_time)=timeline
+                    AND activity_level_id       =activity_sleep
+                    AND subject_device_id       = :subjectDeviceId
+GROUP BY
+          timeline
+        , activity_level_id
+ORDER BY
+          timeline
+;
 
--- get average activity counts per day when multiple subjects present, with 0 count if no activity present
-	@Query(value="select timeline, activity_level_id, round(activity_minutes/cast(subjects as numeric),1) as activity_minutes from "
-			+ "(select timeline, activity_level_id, count(activity_sleep) as activity_minutes, subjects from "
-			+ "(select distinct(date_trunc('day',start_time)) as timeline, a.id as activity_level_id, "
-			+ "count(distinct subject_device_id) as subjects from subject_device_activity_level, activity_level a where "
-			+ "subject_device_id in (:subjectDeviceIds) and a.id < 6 group by timeline, a.id) as r1 left join subject_device_activity_level "
-			+ "on date_trunc('day',start_time) = timeline and activity_level_id = activity_sleep and subject_device_id in (:subjectDeviceIds) "
-			+ "group by timeline, activity_level_id,subjects) r2 order by timeline",
+-- get average activity counts per day when multiple subjects present, with 0
+-- count if no activity present
+SELECT
+          timeline
+        , activity_level_id
+        , ROUND(activity_minutes/CAST(subjects AS NUMERIC),1) AS activity_minutes
+FROM
+          (
+                    SELECT
+                              timeline
+                            , activity_level_id
+                            , COUNT(activity_sleep) AS activity_minutes
+                            , subjects
+                    FROM
+                              (
+                                        SELECT DISTINCT
+                                                  (date_trunc('day',start_time))    AS timeline
+                                                , a.id                              AS activity_level_id
+                                                , COUNT(DISTINCT subject_device_id) AS subjects
+                                        FROM
+                                                  subject_device_activity_level
+                                                , activity_level a
+                                        WHERE
+                                                  subject_device_id IN (:subjectDeviceIds)
+                                                  AND a.id < 6
+                                        GROUP BY
+                                                  timeline
+                                                , a.id
+                              )
+                              AS r1
+                              LEFT JOIN
+                                        subject_device_activity_level
+                              ON
+                                        date_trunc('day',start_time) = timeline
+                                        AND activity_level_id        = activity_sleep
+                                        AND subject_device_id IN (:subjectDeviceIds)
+                              GROUP BY
+                                        timeline
+                                      , activity_level_id
+                                      , subjects
+          )
+          r2
+ORDER BY
+          timeline
+;
 
--- For weekdays (SUN - THU), get average activity counts per hour with one or more subjects present, and 0 count if no activity present
-	@Query(value="select hours, activity_level_id, round(count(activity_sleep)/cast(subjects as numeric),1) as activity_minutes from "
-			+ "(select extract(hours from timeline) as hours, count(subject_device_id) subjects, activity_level_id from "
-			+ "(select distinct date_trunc('hour',start_time) as timeline, subject_device_id, a.id as activity_level_id from "
-			+ "subject_device_activity_level, activity_level a where subject_device_id in (:subjectDeviceIds) and "
-			+ "extract(dow from start_time) < 5 and a.id < 6) r1 group by extract(hours from timeline), activity_level_id) r2 "
-			+ "left join subject_device_activity_level on extract(hours from start_time) = hours and activity_level_id = activity_sleep "
-			+ "and subject_device_id in (:subjectDeviceIds) and extract(dow from start_time) < 5 "
-			+ "group by hours, activity_level_id, subjects order by hours",
+-- For weekdays (SUN - THU), get average activity counts per hour with one or
+-- more subjects present, and 0 count if no activity present
+SELECT
+          hours
+        , activity_level_id
+        , ROUND(COUNT(activity_sleep)/CAST(subjects AS NUMERIC),1) AS activity_minutes
+FROM
+          (
+                    SELECT
+                              extract(hours FROM timeline) AS hours
+                            , COUNT(subject_device_id)        subjects
+                            , activity_level_id
+                    FROM
+                              (
+                                        SELECT DISTINCT
+                                                  date_trunc('hour',start_time) AS timeline
+                                                , subject_device_id
+                                                , a.id AS activity_level_id
+                                        FROM
+                                                  subject_device_activity_level
+                                                , activity_level a
+                                        WHERE
+                                                  subject_device_id IN (:subjectDeviceIds)
+                                                  AND extract(dow FROM start_time) < 5
+                                                  AND a.id                         < 6
+                              )
+                              r1
+                    GROUP BY
+                              extract(hours FROM timeline)
+                            , activity_level_id
+          )
+          r2
+          LEFT JOIN
+                    subject_device_activity_level
+          ON
+                    extract(hours FROM start_time) = hours
+                    AND activity_level_id          = activity_sleep
+                    AND subject_device_id IN (:subjectDeviceIds)
+                    AND extract(dow FROM start_time) < 5
+GROUP BY
+          hours
+        , activity_level_id
+        , subjects
+ORDER BY
+          hours
+;
 
--- For weekend (FRI and SAT), get average activity counts per hour with one or more subjects present, and 0 count if no activity present
-	@Query(value="select hours, activity_level_id, round(count(activity_sleep)/cast(subjects as numeric),1) as activity_minutes from "
-			+ "(select extract(hours from timeline) as hours, count(subject_device_id) subjects, activity_level_id from "
-			+ "(select distinct date_trunc('hour',start_time) as timeline, subject_device_id, a.id as activity_level_id from "
-			+ "subject_device_activity_level, activity_level a where subject_device_id in (:subjectDeviceIds) and "
-			+ "extract(dow from start_time) >= 5 and a.id < 6) r1 group by extract(hours from timeline), activity_level_id) r2 "
-			+ "left join subject_device_activity_level on extract(hours from start_time) = hours and activity_level_id = activity_sleep "
-			+ "and subject_device_id in (:subjectDeviceIds) and extract(dow from start_time) >= 5 "
-			+ "group by hours, activity_level_id, subjects order by hours",
+-- For weekend (FRI and SAT), get average activity counts per hour with one or
+-- more subjects present, and 0 count if no activity present
+SELECT
+          hours
+        , activity_level_id
+        , ROUND(COUNT(activity_sleep)/CAST(subjects AS NUMERIC),1) AS activity_minutes
+FROM
+          (
+                    SELECT
+                              extract(hours FROM timeline) AS hours
+                            , COUNT(subject_device_id)        subjects
+                            , activity_level_id
+                    FROM
+                              (
+                                        SELECT DISTINCT
+                                                  date_trunc('hour',start_time) AS timeline
+                                                , subject_device_id
+                                                , a.id AS activity_level_id
+                                        FROM
+                                                  subject_device_activity_level
+                                                , activity_level a
+                                        WHERE
+                                                  subject_device_id IN (:subjectDeviceIds)
+                                                  AND extract(dow FROM start_time) >= 5
+                                                  AND a.id                          < 6
+                              )
+                              r1
+                    GROUP BY
+                              extract(hours FROM timeline)
+                            , activity_level_id
+          )
+          r2
+          LEFT JOIN
+                    subject_device_activity_level
+          ON
+                    extract(hours FROM start_time) = hours
+                    AND activity_level_id          = activity_sleep
+                    AND subject_device_id IN (:subjectDeviceIds)
+                    AND extract(dow FROM start_time) >= 5
+GROUP BY
+          hours
+        , activity_level_id
+        , subjects
+ORDER BY
+          hours
+;
 
 -- Average activity level per day for individuals
-	@Query(value="select activity_level_id, avg(activity_minutes) as avg_activity from (select timeline, activity_level_id, count(activity_level) "
-			+ "as activity_minutes from (select distinct(date_trunc('day',start_time)) as timeline, a.id as activity_level_id "
-			+ "from subject_device_activity_level s,activity_level a where subject_device_id = :subjectDeviceId and a.id = :activityLevelId) as t "
-			+ "left join subject_device_activity_level on date_trunc('day',start_time)=timeline and activity_level_id=activity_level and "
-			+ "subject_device_id = :subjectDeviceId group by timeline, activity_level_id) as avgperday group by activity_level_id"
+SELECT
+          activity_level_id
+        , AVG(activity_minutes) AS avg_activity
+FROM
+          (
+                    SELECT
+                              timeline
+                            , activity_level_id
+                            , COUNT(activity_level) AS activity_minutes
+                    FROM
+                              (
+                                        SELECT DISTINCT
+                                                  (date_trunc('day',start_time)) AS timeline
+                                                , a.id                           AS activity_level_id
+                                        FROM
+                                                  subject_device_activity_level s
+                                                , activity_level                a
+                                        WHERE
+                                                  subject_device_id = :subjectDeviceId
+                                                  AND a.id          = :activityLevelId
+                              )
+                              AS t
+                              LEFT JOIN
+                                        subject_device_activity_level
+                              ON
+                                        date_trunc('day',start_time)=timeline
+                                        AND activity_level_id       =activity_level
+                                        AND subject_device_id       = :subjectDeviceId
+                    GROUP BY
+                              timeline
+                            , activity_level_id
+          )
+          AS avgperday
+GROUP BY
+          activity_level_i
+;
 
-	@Query(value="select timeline, activity_level_id, round(activity_minutes/cast(subjects as numeric),1) as activity_minutes from "
-			+ "(select timeline, activity_level_id, count(activity_sleep) as activity_minutes, subjects from "
-			+ "(select distinct(date_trunc('day',start_time)) as timeline, a.id as activity_level_id, "
-			+ "count(distinct subject_device_id) as subjects from subject_device_activity_level, activity_level a where "
-			+ "subject_device_id in (:subjectDeviceIds) and a.id < 6 group by timeline, a.id) as r1 left join subject_device_activity_level "
-			+ "on date_trunc('day',start_time) = timeline and activity_level_id = activity_sleep and subject_device_id in (:subjectDeviceIds) "
-			+ "group by timeline, activity_level_id,subjects) r2 order by timeline",
+SELECT
+          timeline
+        , activity_level_id
+        , ROUND(activity_minutes/CAST(subjects AS NUMERIC),1) AS activity_minutes
+FROM
+          (
+                    SELECT
+                              timeline
+                            , activity_level_id
+                            , COUNT(activity_sleep) AS activity_minutes
+                            , subjects
+                    FROM
+                              (
+                                        SELECT DISTINCT
+                                                  (date_trunc('day',start_time))    AS timeline
+                                                , a.id                              AS activity_level_id
+                                                , COUNT(DISTINCT subject_device_id) AS subjects
+                                        FROM
+                                                  subject_device_activity_level
+                                                , activity_level a
+                                        WHERE
+                                                  subject_device_id IN (:subjectDeviceIds)
+                                                  AND a.id < 6
+                                        GROUP BY
+                                                  timeline
+                                                , a.id
+                              )
+                              AS r1
+                              LEFT JOIN
+                                        subject_device_activity_level
+                              ON
+                                        date_trunc('day',start_time) = timeline
+                                        AND activity_level_id        = activity_sleep
+                                        AND subject_device_id IN (:subjectDeviceIds)
+                              GROUP BY
+                                        timeline
+                                      , activity_level_id
+                                      , subjects
+          )
+          r2
+ORDER BY
+          timeline
+;
