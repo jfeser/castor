b8b05496d2873edbbeca2430c8fa61e1eb4137e9
Jack Feser <feser@csail.mit.edu>
2018-04-26 00:24:14 -0400

    Increase database int sizes.

--- bin/create_benchmark_db.sql
+++ bin/create_benchmark_db.sql
@@ -4,16 +4,16 @@ create database benchmarks;
 
 create type state as enum ('unsearched', 'searching', 'searched');
 create table template (
-       hash           integer primary key,
-       program_size   integer,
+       hash           bigint primary key,
+       program_size   bigint,
        valid          boolean,
        search_time    timestamp default localtimestamp,
        search_state   state,
        search_transforms text,
        search_failed  boolean,
        runs_per_sec   numeric,
-       db_size        integer,
-       exe_size       integer,
+       db_size        bigint,
+       exe_size       bigint,
        compile_failed boolean,
        scanner_failed boolean
 );
