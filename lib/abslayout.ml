7908a2f407454c99636ee6cfbe3c7f501bfc1851
Jack Feser <feser@csail.mit.edu>
2018-05-18 16:51:02 -0400

    Add subst method for ralgebra.

--- lib/abslayout.ml
+++ lib/abslayout.ml
@@ -1,6 +1,9 @@
 open Base
+open Stdio
 open Printf
+open Expect_test_helpers_kernel
 
+open Collections
 open Db
 open Layout
 
@@ -18,7 +21,7 @@ type op =
   | Div
   | Mod
 
-type 'f agg =
+and 'f agg =
   | Count
   | Key of 'f
   | Sum of 'f
@@ -26,14 +29,76 @@ type 'f agg =
   | Min of 'f
   | Max of 'f
 
-type ('f, 'l) ralgebra =
+and 'f pred =
+  | Var of Type.TypedName.t
+  | Field of 'f
+  | Null
+  | Int of int
+  | Bool of bool
+  | String of string
+  | Binop of (op * 'f pred * 'f pred)
+  | Varop of (op * 'f pred list)
+
+and ('f, 'l) ralgebra =
   | Project of 'f list * ('f, 'l) ralgebra
-  | Filter of 'f Ralgebra0.pred * ('f, 'l) ralgebra
+  | Filter of 'f pred * ('f, 'l) ralgebra
   | EqJoin of 'f * 'f * ('f, 'l) ralgebra * ('f, 'l) ralgebra
   | Scan of 'l
-  | Concat of ('f, 'l) ralgebra list
-  | Count of ('f, 'l) ralgebra
   | Agg of 'f agg list * 'f list * ('f, 'l) ralgebra
+[@@deriving visitors { variety = "endo" },
+            visitors { variety = "map" },
+            sexp]
+
+let pred_of_value : Db.primvalue -> 'a pred = function
+  | `Bool x -> Bool x
+  | `String x -> String x
+  | `Int x -> Int x
+  | `Null -> Null
+  | `Unknown _ -> failwith ""
+
+let subst : PredCtx.t -> (Field.t, 'a) ralgebra -> (Field.t, 'a) ralgebra =
+  fun ctx ->
+    let v = object
+      inherit [_] endo as super
+
+      method! visit_Var _ this v =
+        match Map.find ctx (Var v) with
+        | Some x -> pred_of_value x
+        | None -> this
+
+      method! visit_Field _ this f =
+        match Map.find ctx (Field f) with
+        | Some x -> pred_of_value x
+        | None -> this
+
+      method visit_t _ x = x
+      method visit_'l _ x = x
+      method visit_'f _ x = x
+    end in
+    v#visit_ralgebra ()
+
+let%expect_test "subst" =
+  let f = Field.of_name "f" in
+  let g = Field.of_name "g" in
+  let r = Relation.of_name "r" in
+  let ctx = Map.of_alist_exn (module PredCtx.Key)
+      [Field f, `Int 1; Field g, `Int 2] in
+  let r = Filter (Binop (Eq, Field f, Field g), Project ([f; g], Scan r)) in
+  print_s ([%sexp_of:(Field.t, Relation.t) ralgebra] (subst ctx r));
+  [%expect {|
+    (Filter
+      (Binop (
+        Eq
+        (Int 1)
+        (Int 2)))
+      (Project
+        (((name  f)
+          (dtype DBool)
+          (relation ((name "") (fields ()))))
+         ((name  g)
+          (dtype DBool)
+          (relation ((name "") (fields ())))))
+        (Scan ((name r) (fields ()))))) |}]
 
 type 'a lambda = string * 'a
 
@@ -51,17 +116,18 @@ type t =
   | AEmpty
   | AScalar of Field.t Ralgebra0.pred
   | AList of (Relation.t, Field.t) ralgebra * t lambda
-  | ATuple of [`Cross | `Zip] * t list lambda
+  | ATuple of [`Cross | `Zip] * t list
   | AHashIdx of (Relation.t, Field.t) ralgebra * t list lambda * hash_idx
   | AOrderedIdx of (Relation.t, Field.t) ralgebra * t list lambda * ordered_idx
 
-let rec pred_to_sql : string Ralgebra0.pred -> string = function
-  | Var _ as x -> Error.create "Unsupported." x [%sexp_of:string Ralgebra0.pred] |> Error.raise
+let rec pred_to_sql : string pred -> string = function
+  | Var _ as x -> Error.create "Unsupported." x [%sexp_of:string pred] |> Error.raise
   | Field f -> sprintf "\"%s\"" f
   | Int x -> Int.to_string x
   | Bool true -> "true"
   | Bool false -> "false"
   | String s -> sprintf "'%S'" s
+  | Null -> "null"
   | Binop (op, p1, p2) ->
     let s1 = sprintf "(%s)" (pred_to_sql p1) in
     let s2 = sprintf "(%s)" (pred_to_sql p2) in
@@ -108,10 +174,14 @@ let ralgebra_to_sql : (Field.t, Relation.t) ralgebra -> string = fun r ->
       let table = fresh () in
       let pred =
         let f = object
-          inherit ['f1, 'f2] Ralgebra0.pred_map as super
-          method field (f : Field.t) = sprintf "t%d.%s" table f.name
+          inherit [_] map as super
+          method visit_Field _ (f : Field.t) =
+            Field (sprintf "t%d.%s" table f.name)
+          method visit_t = failwith "Unused"
+          method visit_'l = failwith "Unused"
+          method visit_'f = failwith "Unused"
         end in
-        f#run pred
+        f#visit_pred () pred
       in
       sprintf "select * from (%s) as t%d where %s" (f r) table (pred_to_sql pred)
     | EqJoin (f1, f2, r1, r2) ->
@@ -119,7 +189,6 @@ let ralgebra_to_sql : (Field.t, Relation.t) ralgebra -> string = fun r ->
       let t2 = fresh () in
       sprintf "select * from (%s) as t%d, (%s) as t%d where t%d.%s = t%d.%s"
         (f r1) t1 (f r2) t2 t1 f1.name t2 f2.name
-    | Count r -> sprintf "select count(*) from (%s)" (f r)
     | Agg (aggs, key, r) ->
       let table = fresh () in
       let aggs = List.map aggs ~f:(function
@@ -136,7 +205,6 @@ let ralgebra_to_sql : (Field.t, Relation.t) ralgebra -> string = fun r ->
         |> String.concat ~sep:", "
       in
       sprintf "select %s from (%s) as t%d group by (%s)" aggs (f r) table key
-    | Concat _ -> failwith "Unsupported."
   in
   f r
 
@@ -169,81 +237,30 @@ let%expect_test "agg" =
   print_endline (ralgebra_to_sql r);
   [%expect {| select sum(t1.g) from (select * from r) as t1 group by (t1.f, t1.g) |}]
 
-(* let eval_relation : Postgresql.connection -> Field.t list -> string -> Tuple.t list =
- *   fun conn fields query ->
- *     exec ~verbose:false conn query
- *     |> List.mapi ~f:(fun i vs ->
- *         let m_values = List.map2 vs fields ~f:(fun v f ->
- *             let pval = if String.(v = "") then `Null else
- *                 match f.dtype with
- *                 | DInt -> `Int (Int.of_string v)
- *                 | DString -> `String v
- *                 | DBool ->
- *                   begin match v with
- *                     | "t" -> `Bool true
- *                     | "f" -> `Bool false
- *                     | _ -> failwith "Unknown boolean value."
- *                   end
- *                 | _ -> `Unknown v
- *             in
- *             let value = Value.({ rel = Relation.dummy; field = f; value = pval }) in
- *             value)
- *         in
- *         match m_values with
- *         | Ok v -> v
- *         | Unequal_lengths ->
- *           Error.(create "Unexpected tuple width." (List.length fields, List.length vs)
- *                    [%sexp_of:int * int] |> raise))
- * 
- * let to_layout : Postgresql.connection -> t -> Layout.t = fun conn al ->
- *   let rec f tf = function
- *     | Tuple fs ->
- *       let query =
- *         List.map fs ~f:(fun f -> sprintf "%s.%s" f.relation.name f.name)
- *         |> String.concat ~sep:", "
- *         |> (fun fs_str -> sprintf "select %s from %s" fs_str
- *                (List.hd_exn fs).relation.name)
- *         |> tf
- *       in
- *       eval_relation conn fs query
- *       |> List.map ~f:(fun tup -> cross_tuple (List.map ~f:(fun v -> of_value v) tup))
- *       |> unordered_list
- *     | Table (value, ({ field = key; lookup } as t)) ->
- *       let key_query = sprintf "select %s from %s" key.name key.relation.name |> tf in
- *       let keys = eval_relation conn [key] key_query |> List.map ~f:(fun [x] -> x) in
- *       let map = List.map keys ~f:(fun key_value ->
- *           let value_tf q =
- *             sprintf "select * from (%s) as t where %s = %s"
- *               q key.name (Value.to_sql key_value)
- *             |> tf
- *           in
- *           key_value, f value_tf value)
- *                 |> Map.of_alist_exn (module ValueMap.Elem)
- *       in
- *       table map t
- *     | Grouping (value, ({ key; } as t)) ->
- *       let key_query =
- *         List.map key ~f:(fun f -> sprintf "%s.%s" f.relation.name f.name)
- *         |> String.concat ~sep:", "
- *         |> (fun fs_str -> sprintf "select %s from %s" fs_str
- *                (List.hd_exn key).relation.name)
- *         |> tf
- * 
- *         sprintf "select %s from %s" key.name key.relation.name |> tf in
- *       let keys = eval_relation conn [key] key_query |> List.map ~f:(fun [x] -> x) in
- *       let map = List.map keys ~f:(fun key_value ->
- *           let value_tf q =
- *             sprintf "select * from (%s) as t where %s = %s"
- *               q key.name (Value.to_sql key_value)
- *             |> tf
- *           in
- *           key_value, f value_tf value)
- *                 |> Map.of_alist_exn (module ValueMap.Elem)
- *       in
- *       table map t
- * 
- *   in
- *   f (fun x -> x) al
- * 
- * let partition : PredCtx.Key.t -> Field.t -> t -> t = fun k f l ->
- *   Table (l, { field = f; lookup = k }) *)
+(* let subst : PredCtx.t -> (Relation.t, Field.t) ralgebra -> (Relation.t, Field.t) ralgebra = fun ctx r -> *)
+
+
+module Config = struct
+  module type S = sig
+    include Eval.Config.S
+  end
+end
+
+module Make (Config : Config.S) = struct
+  module Eval = Eval.Make(Config)
+    (* let eval : PredCtx.t -> (Relation.t, Field.t) ralgebra -> Tuple.t Seq.t =
+     *   fun ctx query ->
+     *     let query = subst *)
+
+  (* let rec materialize : PredCtx.t -> t -> Layout.t = fun ctx -> function
+   *   | AEmpty -> empty
+   *   | AScalar e ->
+   *     Layout.of_value {
+   *       value = Eval.eval_pred ctx e; rel = Relation.dummy; field = Field.dummy
+   *     }
+   *   | AList (q, l) -> Seq.map (eval_ralgebra ctx q) ~f:(apply l)
+   *   | ATuple (`Zip, ls) -> zip_tuple (List.map ~f:(materialize ctx) ls)
+   *   | ATuple (`Cross, ls) -> cross_tuple (List.map ~f:(materialize ctx) ls)
+   *   | AHashIdx (q, _, _)
+   *   | AOrderedIdx (_, _, _) *)
+end
