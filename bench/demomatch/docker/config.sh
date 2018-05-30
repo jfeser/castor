68e1665f678258ebd474d2ba3634754bc8902452
John K. Feser <jack.feser@gmail.com>
2018-05-30 16:59:10 -0400

    Also load metadata.

--- bench/demomatch/docker/config.sh
+++ bench/demomatch/docker/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+createdb demomatch;
 while read DB_NAME; do
-    createdb "$DB_NAME";
-    pgloader mysql://root:password@172.17.0.2/$DB_NAME postgresql://$POSTGRES_USER@unix:/var/run/postgresql:5432/$DB_NAME;
+    pgloader mysql://root:password@172.17.0.2/$DB_NAME postgresql://$POSTGRES_USER@unix:/var/run/postgresql:5432/demomatch;
 done < /docker-entrypoint-initdb.d/db-names.txt
