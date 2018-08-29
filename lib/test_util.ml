ea5e958b69f8cf972beb830de92da39a33e97de5
Jack Feser <feser@csail.mit.edu>
2018-08-29 10:55:43 -0400

    Refactor tests.

---
+++ lib/test_util.ml
@@ -0,0 +1,15 @@
+open Base
+open Abslayout
+
+let create rels name fs xs =
+  let rel =
+    Db.{rname= name; fields= List.map fs ~f:(fun f -> {fname= f; dtype= DInt})}
+  in
+  let data =
+    List.map xs ~f:(fun data ->
+        List.map2_exn fs data ~f:(fun fname value -> (fname, `Int value)) )
+  in
+  Hashtbl.set rels ~key:rel ~data ;
+  ( name
+  , List.map fs ~f:(fun f ->
+        Name.{name= f; relation= Some name; type_= Some Type0.PrimType.IntT} ) )
