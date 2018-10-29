14e55285540c2a81b80ca1faea80f06194790576
Jack Feser <jack.feser@gmail.com>
2018-10-29 13:57:07 -0400

    Add code to extract hyper results.

---
+++ bench/tpch-hyper/extract-results.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''Usage: extract-results.py LOG'''
+
+import re
+from docopt import docopt
+
+def main(args):
+    with open(args['LOG'], 'r') as f:
+        text = f.read()
+
+    print('name,runtime')
+    regex = re.compile(r".*'(\d+[a-zA-Z-]*).*?([0-9]+)ms exe.*")
+    for line in text.split('\n'):
+        match = regex.fullmatch(line)
+        if match is not None:
+            bench_name = match.groups()[0]
+            run_time = match.groups()[1]
+            print('%s,%s' % (bench_name, run_time))
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    main(args)
