630bb3f18b887abd9de8073905d55d1829d42bb0
Jack Feser <feser@csail.mit.edu>
2018-11-02 17:47:48 -0400

    Add queries for unspecialized hyper.

---
+++ bench/tpch-hyper-unspec/run_bench.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DRIVER_PATH=../../../hyperdemo/bin/driver
+
+echo "Dumping data from Postgres..."
+psql -d tpch -f dump.sql
+
+echo "Creating database..."
+$DRIVER_PATH schema.sql load.sql --store db.dump
+
+echo "Running queries..."
+$DRIVER_PATH db.dump -r 100 -b "1.sql" "2.sql" "3-no.sql" "4.sql" "6.sql" 2>&1 | tee hyper-output.log
