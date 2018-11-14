4a030d360c5c2b9303974cbcdbad4ed431c60e96
Jack Feser <jack.feser@gmail.com>
2018-11-13 22:22:42 -0500

    Fix hyper specialized queries.

--- bench/tpch-hyper/4.sql
+++ bench/tpch-hyper/4.sql
@@ -2,4 +2,5 @@ select o_orderpriority, sum(agg2) as order_count
   from q4
  where o_orderdate >= date '1993-07-01'
 	 and o_orderdate < date '1993-07-01' + interval '3' month
+   group by o_orderpriority
  order by o_orderpriority;
