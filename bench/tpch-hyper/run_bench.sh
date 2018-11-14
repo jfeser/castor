c03a0af48b53b5c106d492a7f9149f8584e8ed93
Jack Feser <jack.feser@gmail.com>
2018-11-13 22:21:52 -0500

    Update hyper run script.

--- bench/tpch-hyper/run_bench.sh
+++ bench/tpch-hyper/run_bench.sh
@@ -6,7 +6,7 @@ echo "Dumping data from Postgres..."
 psql -d tpch -f dump.sql
 
 echo "Creating database..."
-$DRIVER_PATH schema.sql load.sql --store db.dump
+$DRIVER_PATH schema.sql load.sql --store db.dump 2>&1 | tee hyper-db-create.log
 
 echo "Running queries..."
 $DRIVER_PATH db.dump -r 100 -b "1.sql" "2.sql" "3-no.sql" "4.sql" "6.sql" 2>&1 | tee hyper-output.log
