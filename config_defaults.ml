2306c7930a67153bd4f4cd09ff9682c27cc320c9
Jack Feser <feser@csail.mit.edu>
2019-01-07 14:14:14 +0900

    Fix config defaults.

--- config_defaults.ml
+++ config_defaults.ml
@@ -2,4 +2,4 @@
 let tpch_db = "postgresql://localhost:5432/tpch"
 
 (** The database to use when running Demomatch benchmarks. *)
-let demomatch_db = "postgresql://localhost:5433/demomatch"
+let demomatch_db = "postgresql://localhost:5432/demomatch"
