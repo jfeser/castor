f3266f6154c7384ba17331feef8e556dc88a6d25
Jack Feser <feser@csail.mit.edu>
2019-01-13 15:06:12 +0900

    Remove unused benchmark files.

--- bench/demomatch/docker/config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-createdb demomatch;
-while read DB_NAME; do
-    pgloader mysql://root:password@172.17.0.2/$DB_NAME postgresql://$POSTGRES_USER@unix:/var/run/postgresql:5432/demomatch;
-done < /docker-entrypoint-initdb.d/db-names.txt
