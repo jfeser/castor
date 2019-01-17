0096cf5fd3597a482cf5f729b4f1bb2b60bb1c15
Jack Feser <feser@csail.mit.edu>
2019-01-17 19:20:19 +0900

    Replace scary cstdlib date math with scarier c++ date math.

---
+++ etc/date.h
@@ -0,0 +1,73 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015, 2016, 2017 Howard Hinnant
+// Copyright (c) 2015 Ville Voutilainen
+// Copyright (c) 2016 Alexander Kormanovsky
+// Copyright (c) 2016, 2017 Jiangang Zhuang
+// Copyright (c) 2017 Nicolas Veloz Savino
+// Copyright (c) 2017 Florian Dang
+// Copyright (c) 2017 Aaron Bishop
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Adapted from https://github.com/HowardHinnant/date
+
+#ifndef DATE_H
+#define DATE_H
+
+// Returns year/month/day triple in civil calendar
+// Preconditions:  z is number of days since 1970-01-01 and is in the range:
+//                   [numeric_limits<Int>::min(), numeric_limits<Int>::max()-719468].
+inline void civil_from_days(int z, int* year, int* mon, int* day)
+{
+    z += 719468;
+    const int era = (z >= 0 ? z : z - 146096) / 146097;
+    const unsigned doe = (z - era * 146097);          // [0, 146096]
+    const unsigned yoe = (doe - doe/1460 + doe/36524 - doe/146096) / 365;  // [0, 399]
+    const int y = (yoe) + era * 400;
+    const unsigned doy = doe - (365*yoe + yoe/4 - yoe/100);                // [0, 365]
+    const unsigned mp = (5*doy + 2)/153;                                   // [0, 11]
+    const unsigned d = doy - (153*mp+2)/5 + 1;                             // [1, 31]
+    const unsigned m = mp + (mp < 10 ? 3 : -9);                            // [1, 12]
+    *year = y + (m <= 2);
+    *mon = m;
+    *day = d;
+}
+
+// Returns number of days since civil 1970-01-01.  Negative values indicate
+//    days prior to 1970-01-01.
+// Preconditions:  y-m-d represents a date in the civil (Gregorian) calendar
+//                 m is in [1, 12]
+//                 d is in [1, last_day_of_month(y, m)]
+//                 y is "approximately" in
+//                   [numeric_limits<Int>::min()/366, numeric_limits<Int>::max()/366]
+//                 Exact range of validity is:
+//                 [civil_from_days(numeric_limits<Int>::min()),
+//                  civil_from_days(numeric_limits<Int>::max()-719468)]
+inline int days_from_civil(int y, unsigned m, unsigned d) 
+{
+    y -= m <= 2;
+    const int era = (y >= 0 ? y : y-399) / 400;
+    const unsigned yoe = (y - era * 400);      // [0, 399]
+    const unsigned doy = (153*(m + (m > 2 ? -3 : 9)) + 2)/5 + d-1;  // [0, 365]
+    const unsigned doe = yoe * 365 + yoe/4 - yoe/100 + doy;         // [0, 146096]
+    return era * 146097 + doe - 719468;
+}
+
+#endif
--- etc/stdlib.c
+++ etc/stdlib.c
@@ -1,10 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-
-// Divide out the number of seconds, rounding up.
-#define to_days(t) (((t) + 86400 - 1) / 86400)
-#define to_secs(t) ((t) * 86400)
+#include "date.h"
 
 long strpos(char* s1, long l1, char* s2, long l2) {
   if (l1 < l2) { return 0; }
@@ -19,43 +16,36 @@ long strpos(char* s1, long l1, char* s2, long l2) {
 }
 
 long extract_year(long date) {
-  time_t time = date * 24 * 60 * 60;
-  struct tm btime;
-  gmtime_r(&time, &btime);
-  // tm_year is the number of years since 1900
-  return btime.tm_year + 1900;
+  int y, m, d;
+  civil_from_days(date, &y, &m, &d);
+  return y;
 }
 
 long extract_month(long date) {
-  time_t time = date * 24 * 60 * 60;
-  struct tm btime;
-  gmtime_r(&time, &btime);
-  // tm_mon ranges from 0 to 11
-  return btime.tm_mon + 1;
+  int y, m, d;
+  civil_from_days(date, &y, &m, &d);
+  return m;
 }
 
 long extract_day(long date) {
-  time_t time = date * 24 * 60 * 60;
-  struct tm btime;
-  gmtime_r(&time, &btime);
-  // tm_day is an ordinal unlike everything else
-  return btime.tm_mday;
+  int y, m, d;
+  civil_from_days(date, &y, &m, &d);
+  return d;
 }
 
 long add_year(long date, long years) {
-  struct tm bdate;
-  time_t secs = to_secs(date);
-  gmtime_r(&secs, &bdate);
-  bdate.tm_year += years;
-  return to_days(mktime(&bdate));
+  int y, m, d;
+  civil_from_days(date, &y, &m, &d);
+  y += years;
+  return days_from_civil(y, m, d);
 }
 
 long add_month(long date, long months) {
-  struct tm bdate;
-  time_t secs = to_secs(date);
-  gmtime_r(&secs, &bdate);
-  bdate.tm_mon += months;
-  return to_days(mktime(&bdate));
+  int y, m, d;
+  civil_from_days(date, &y, &m, &d);
+  y += months / 12;
+  m += months % 12;
+  return days_from_civil(y, m, d);
 }
 
 long load_date(char* s, long* out) {
@@ -63,6 +53,9 @@ long load_date(char* s, long* out) {
   if (strptime(s, "%Y-%m-%d", &time_tm) == NULL) {
     return 1;
   }
-  *out = to_days(mktime(&time_tm));
+  // tm_year is the number of years since 1900
+  // tm_mon ranges from 0 to 11
+  // tm_day is an ordinal, unlike everything else
+  *out = days_from_civil(time_tm.tm_year + 1900, time_tm.tm_mon + 1, time_tm.tm_mday);
   return 0;
 }
