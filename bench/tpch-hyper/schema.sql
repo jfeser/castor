7252c4ac9849aa5102bda92a715072d01bb8833b
Jack Feser <feser@csail.mit.edu>
2019-01-18 17:50:33 +0900

    Reenable q10 tables.

--- bench/tpch-hyper/schema.sql
+++ bench/tpch-hyper/schema.sql
@@ -114,24 +114,24 @@ CREATE TABLE q9 (
     p_name varchar(55) NOT NULL
 );
 
--- CREATE TABLE q10_1 (
---     revenue numeric (16, 6) NOT NULL,
---     n_name char(25) NOT NULL,
---     c_custkey integer NOT NULL
--- );
--- CREATE TABLE q10_2 (
---     c_custkey integer NOT NULL,
---     c_name varchar(25) NOT NULL,
---     c_address varchar(40) NOT NULL,
---     c_nationkey integer NOT NULL,
---     c_phone char(15) NOT NULL,
---     c_acctbal decimal (12,
---         2) NOT NULL,
---     c_mktsegment char(10) NOT NULL,
---     c_comment varchar(117) NOT NULL
--- );
--- CREATE INDEX q10_1_idx ON q10_1 (o_orderdate);
--- CREATE INDEX q10_2_idx ON q10_2 (c_custkey);
+CREATE TABLE q10_1 (
+    revenue numeric (16, 6) NOT NULL,
+    n_name char(25) NOT NULL,
+    c_custkey integer NOT NULL
+);
+CREATE TABLE q10_2 (
+    c_custkey integer NOT NULL,
+    c_name varchar(25) NOT NULL,
+    c_address varchar(40) NOT NULL,
+    c_nationkey integer NOT NULL,
+    c_phone char(15) NOT NULL,
+    c_acctbal decimal (12,
+        2) NOT NULL,
+    c_mktsegment char(10) NOT NULL,
+    c_comment varchar(117) NOT NULL
+);
+CREATE INDEX q10_1_idx ON q10_1 (o_orderdate);
+CREATE INDEX q10_2_idx ON q10_2 (c_custkey);
 
 CREATE TABLE q11_1 (
     n_name char(25) NOT NULL,
