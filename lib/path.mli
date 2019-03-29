0f45a9330691894fc526bf808ce2f34f9765b224
Jack Feser <feser@csail.mit.edu>
2019-03-29 15:41:26 -0400

    Hide path type.

---
+++ lib/path.mli
@@ -0,0 +1,25 @@
+open Collections
+
+type t [@@deriving compare, sexp]
+
+val shallowest_first : t -> t -> int
+
+val deepest_first : t -> t -> int
+
+val root : t
+
+val length : t -> int
+
+val set_exn : t -> Abslayout.t -> Abslayout.t -> Abslayout.t
+
+val get_exn : t -> Abslayout.t -> Abslayout.t
+
+val all : Abslayout.t -> t Seq.t
+
+val is_run_time : Abslayout.t -> t -> bool
+
+val is_compile_time : Abslayout.t -> t -> bool
+
+val parent : t -> t option
+
+val child : t -> int -> t
