27bcf4ac5c93258ee7dd1ec1436b1bff61734fa9
Jack Feser <feser@csail.mit.edu>
2019-06-17 19:06:59 -0400

    Add result extraction script.

---
+++ bin/results.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import csv
+from glob import glob
+import os
+import sys
+
+def read_time(bench_dir, name):
+    with open('%s/%s.time' % (bench_dir, name)) as f:
+        output = f.read()
+    time_str = output.split(" ")[0][:-2]
+    time = None
+    try:
+        time = float(time_str)
+    except ValueError:
+        print("Error: failed to read time: %s" % time_str, file=sys.stderr)
+    return time
+
+def read_size(bench_dir, name):
+    bin_fn = '%s/%s/data.bin' % (bench_dir, name)
+    size = None
+    try:
+        size = os.stat(bin_fn).st_size
+    except FileNotFoundError:
+        print("Error: failed to read size: %s" % name, file=sys.stderr)
+    return size
+
+def main(bench_dir):
+    csv_writer = csv.writer(sys.stdout)
+    csv_writer.writerow(['name', 'time', 'size'])
+    for fn in glob('%s/*.time' % bench_dir):
+        name = os.path.split(fn)[1].split('.')[0]
+        time = read_time(bench_dir, name)
+        size = read_size(bench_dir, name)
+        csv_writer.writerow([name, time, size])
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: results.py BENCH_DIR')
+        exit(1)
+
+    main(sys.argv[1])
