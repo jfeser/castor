5a8932bcd94f00878301b7f45a406563f61bf617
Jack Feser <feser@csail.mit.edu>
2019-08-20 14:02:33 -0700

    Add memory stats to results script.

--- bin/results.py
+++ bin/results.py
@@ -25,14 +25,27 @@ def read_size(bench_dir, name):
         print("Error: failed to read size: %s" % name, file=sys.stderr)
     return size
 
+def read_mem(bench_dir, name):
+    try:
+        with open('%s/%s.mem' % (bench_dir, name)) as f:
+            for line in f:
+                if 'Maximum resident set size' in line:
+                    mem = int(line.split(':')[1])
+                    return mem
+            else:
+                print('Error: Failed to read mem.')
+    except FileNotFoundError:
+        print('Error failed to read mem.')
+
 def main(bench_dir):
     csv_writer = csv.writer(sys.stdout)
-    csv_writer.writerow(['name', 'time', 'size'])
+    csv_writer.writerow(['name', 'time', 'size', 'mem'])
     for fn in glob('%s/*.time' % bench_dir):
         name = os.path.split(fn)[1].split('.')[0]
         time = read_time(bench_dir, name)
         size = read_size(bench_dir, name)
-        csv_writer.writerow([name, time, size])
+        mem = read_mem(bench_dir, name)
+        csv_writer.writerow([name, time, size, mem])
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
