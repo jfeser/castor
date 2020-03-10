9cd74e4e33404b84b02ffe1f086dd37f4f664e35
Jack Feser <feser@csail.mit.edu>
2020-03-09 21:29:43 -0400

    Extend sql tool.

--- bin/sql.ml
+++ bin/sql.ml
@@ -5,17 +5,19 @@ open Castor_opt
 open Abslayout_load
 module A = Abslayout
 
-let main ~params:all_params ~simplify ~project ~unnest ~sql ch =
+let main ~params:all_params ~simplify ~project ~unnest ~sql ~cse ch =
   Logs.set_level (Some Debug);
   Logs.Src.set_level Log.src (Some Debug);
+  Format.set_margin 120;
+
   let params =
     List.map all_params ~f:(fun (n, t, _) -> Name.create ~type_:t n)
     |> Set.of_list (module Name)
-  in
-  let param_ctx =
+  and param_ctx =
     List.map all_params ~f:(fun (n, t, v) -> (Name.create ~type_:t n, v))
     |> Map.of_alist_exn (module Name)
   in
+
   let module Config = struct
     let conn = Db.create (Sys.getenv_exn "CASTOR_DB")
 
@@ -27,16 +29,50 @@ let main ~params:all_params ~simplify ~project ~unnest ~sql ch =
   end in
   let module S = Simplify_tactic.Make (Config) in
   let module O = Ops.Make (Config) in
+  let simplify q =
+    if simplify then
+      let q =
+        Cardinality.annotate ~dedup:true q
+        |> Join_elim.remove_joins |> Unnest.hoist_meta
+        |> Cardinality.extend ~dedup:true
+        |> Join_elim.remove_dedup |> A.strip_meta
+      in
+      Option.value_exn (O.apply S.simplify Path.root q)
+    else q
+  in
+
   let query_str = In_channel.input_all ch in
   let query = load_string ~params Config.conn query_str in
-  let query = if unnest then Unnest.unnest query |> A.strip_meta else query in
+  let query = simplify query in
   let query =
-    if simplify then Option.value_exn (O.apply S.simplify Path.root query)
+    let q =
+      if unnest then Unnest.unnest query
+      else
+        ( Cardinality.annotate query
+          :> < cardinality_matters : bool ; why_card_matters : string >
+             Ast.annot )
+    in
+    Cardinality.extend ~dedup:true q
+    |> Join_elim.remove_joins |> Unnest.hoist_meta
+    |> Cardinality.extend ~dedup:true
+    |> Join_elim.remove_dedup |> A.strip_meta
+  in
+  let query = simplify query in
+  let query = if project then Project.project query else query in
+  let query = simplify query in
+  let query =
+    if cse then `Sql (Cse.extract_common query |> Cse.to_sql) else `Query query
+  in
+  let query =
+    if sql then
+      match query with
+      | `Sql _ -> query
+      | `Query q -> `Sql (Sql.of_ralgebra q |> Sql.to_string)
     else query
   in
-  let query = if project then Project.project query else query in
-  if sql then Sql.of_ralgebra query |> Sql.to_string_hum |> print_endline
-  else Format.printf "%a@." Abslayout.pp query
+  match query with
+  | `Sql x -> Sql.format x |> print_endline
+  | `Query x -> Format.printf "%a@." Abslayout.pp x
 
 let () =
   let open Command.Let_syntax in
@@ -50,6 +86,7 @@ let () =
         flag "unnest" ~aliases:[ "u" ] no_arg ~doc:"unnest before simplifying"
       and project =
         flag "project" ~aliases:[ "r" ] no_arg ~doc:"project the query"
+      and cse = flag "cse" ~aliases:[ "c" ] no_arg ~doc:"apply cse"
       and params =
         flag "param" ~aliases:[ "p" ]
           (listed Util.param_and_value)
@@ -57,5 +94,5 @@ let () =
       and ch =
         anon (maybe_with_default In_channel.stdin ("query" %: Util.channel))
       in
-      fun () -> main ~params ~simplify ~project ~unnest ~sql ch]
+      fun () -> main ~params ~simplify ~project ~unnest ~sql ~cse ch]
   |> Command.run
