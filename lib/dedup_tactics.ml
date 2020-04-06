0c7ab24a49c9a40fd68fcc74598c51cf965d123e
Jack Feser <feser@csail.mit.edu>
2020-04-06 13:29:58 -0400

    Fix correctness bug in dedup pushing.

--- lib/dedup_tactics.ml
+++ lib/dedup_tactics.ml
@@ -25,6 +25,11 @@ module Make (C : Config.S) = struct
 
   let elim_dedup = O.of_func elim_dedup ~name:"elim-dedup"
 
+  let lhs_visible lhs rhs =
+    Set.is_subset
+      (Set.of_list (module Name) (Schema.schema lhs))
+      (Set.of_list (module Name) (Schema.schema rhs))
+
   let push_dedup r =
     let open Option.Let_syntax in
     let%bind r = to_dedup r in
@@ -32,10 +37,12 @@ module Make (C : Config.S) = struct
     | Filter (p, r') -> Some (filter p (dedup r'))
     | Dedup r' -> Some (dedup r')
     | AScalar _ | AEmpty -> Some r
-    | AHashIdx h -> Some (hash_idx' { h with hi_values = dedup h.hi_values })
-    | AList l ->
-        return
-        @@ list' { l with l_keys = dedup l.l_keys; l_values = dedup l.l_values }
+    | AHashIdx h -> return @@ hash_idx' { h with hi_values = dedup h.hi_values }
+    | AList ({ l_keys = lhs; l_values = rhs; _ } as l) when lhs_visible lhs rhs
+      ->
+        return @@ list' { l with l_keys = dedup lhs; l_values = dedup rhs }
+    | DepJoin ({ d_lhs = lhs; d_rhs = rhs; _ } as d) when lhs_visible lhs rhs ->
+        return @@ dep_join' { d with d_lhs = dedup lhs; d_rhs = dedup rhs }
     | AOrderedIdx o ->
         (* TODO: This transform isn't correct unless the right hand sides are
            non-overlapping. *)
