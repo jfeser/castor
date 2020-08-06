b3f8c1da8d7c6bcf6a7b0d738d58585320f1761b
Jack Feser <feser@csail.mit.edu>
2020-08-06 11:33:43 -0400

    Clean out unused files.

--- bench/tpch/dump-indexes.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import subprocess
-
-if __name__ == '__main__':
-    out = subprocess.check_output('''psql -t -d tpch -c "select indexdef from pg_indexes where schemaname='public'"''', shell=True)
-    for line in out.strip().splitlines():
-        print(line.decode('utf-8').replace('INDEX', 'INDEX IF NOT EXISTS') + ';')
--- bench/tpch/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf /tmp/tpch-dbgen;
-git clone https://github.com/jfeser/tpch-dbgen.git /tmp/tpch-dbgen;
-cd /tmp/tpch-dbgen || exit;
-make;
-./build.sh;
-psql -d tpch -f indexes.sql
