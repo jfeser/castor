32a65e8307cf60618eae8f443282cc53696bdde5
Jack Feser <feser@csail.mit.edu>
2020-09-04 19:55:39 -0400

    More match functions.

--- lib/match.ml
+++ lib/match.ml
@@ -1,6 +1,11 @@
 open Ast
 
+let to_dedup r = match r.node with Dedup r -> Some r | _ -> None
+
 let to_join r =
   match r.node with Join { pred; r1; r2 } -> Some (pred, r1, r2) | _ -> None
 
 let to_filter r = match r.node with Filter (p, r) -> Some (p, r) | _ -> None
+
+let to_groupby r =
+  match r.node with GroupBy (p, k, r) -> Some (p, k, r) | _ -> None
