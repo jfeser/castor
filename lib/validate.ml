5e39b1256851fd50cded7010a4e6b37919d272d9
Jack Feser <feser@csail.mit.edu>
2020-09-08 12:43:42 -0400

    Remove unused Validate.equiv.

--- lib/validate.ml
+++ lib/validate.ml
@@ -22,41 +22,6 @@ let compare t1 t2 =
   | (Error _ as e), Ok _ | Ok _, (Error _ as e) -> e
   | Error e1, Error e2 -> Error (Error.of_list [ e1; e2 ])
 
-let equiv conn r1 r2 =
-  let r1 = strip_meta r1 in
-  let r2 = strip_meta r2 in
-  if Abslayout.O.(r1 = r2) then Ok ()
-  else
-    let s1 = schema r1 in
-    let s2 = schema r2 in
-    if not ([%compare.equal: Schema.t] s1 s2) then
-      error "Schemas do not match." (s1, s2) [%sexp_of: Schema.t * Schema.t]
-    else
-      let r1 = normal_order r1 in
-      let r2 = normal_order r2 in
-      let ts1 = Db.Async.exec conn r1 in
-      let ts2 = Db.Async.exec conn r2 in
-      let rec check () =
-        let%lwt t1 = Lwt_stream.get ts1 in
-        let%lwt t2 = Lwt_stream.get ts2 in
-        match (t1, t2) with
-        | Some t1, Some t2 ->
-            let cmp = compare t1 t2 in
-            if Or_error.is_ok cmp then check () else return cmp
-        | Some t, None ->
-            return
-            @@ Or_error.(
-                 to_err t >>= fun t ->
-                 Or_error.error "Extra tuple on LHS." t [%sexp_of: tuple list])
-        | None, Some t ->
-            return
-            @@ Or_error.(
-                 to_err t >>= fun t ->
-                 Or_error.error "Extra tuple on RHS." t [%sexp_of: tuple list])
-        | None, None -> return @@ Ok ()
-      in
-      Lwt_main.run (check ())
-
 let shadow_check r =
   let relations_visitor =
     object
